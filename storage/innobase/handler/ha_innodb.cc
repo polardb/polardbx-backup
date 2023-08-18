polardb/polardbx-engine

[Fix] Variables innodb_undo_retention take effect synchronously
Summary
=======

Adjust update function to start undo retention synchronously when
set variable.

storage/innobase/handler/ha_innodb.cc
@@ -700,7 +700,8 @@ static PSI_mutex_info all_innodb_mutexes[] = {
     PSI_MUTEX_KEY(lizard_sys_mtx_id_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(gp_sys_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(gp_sys_wait_mutex, 0, 0, PSI_DOCUMENT_ME),
-    PSI_MUTEX_KEY(file_purge_list_mutex, 0, 0, PSI_DOCUMENT_ME)};
+    PSI_MUTEX_KEY(file_purge_list_mutex, 0, 0, PSI_DOCUMENT_ME),
+    PSI_MUTEX_KEY(lizard_undo_retention_mutex, 0, 0, PSI_DOCUMENT_ME)};
 #endif /* UNIV_PFS_MUTEX */
 
 #ifdef UNIV_PFS_RWLOCK
@@ -22580,7 +22581,7 @@ static MYSQL_SYSVAR_ULONG(undo_retention,
                           lizard::Undo_retention::retention_time,
                           PLUGIN_VAR_OPCMDARG,
                           "Retention time of undo data in seconds", NULL,
-                          lizard::Undo_retention::on_update,
+                          lizard::Undo_retention::on_update_and_start,
                           0, 0, UINT_MAX32, 0);
 
 static MYSQL_SYSVAR_ULONG(undo_space_supremum_size,

polardb/polardbx-backup

storage/innobase/handler/ha_innodb.cc
@@ -799,7 +799,8 @@ static PSI_mutex_info all_innodb_mutexes[] = {
     PSI_MUTEX_KEY(lizard_vision_list_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(lizard_sys_mtx_id_mutex, 0, 0, PSI_DOCUMENT_ME),
     PSI_MUTEX_KEY(gp_sys_mutex, 0, 0, PSI_DOCUMENT_ME),
-    PSI_MUTEX_KEY(gp_sys_wait_mutex, 0, 0, PSI_DOCUMENT_ME)};
+    PSI_MUTEX_KEY(gp_sys_wait_mutex, 0, 0, PSI_DOCUMENT_ME),
+    PSI_MUTEX_KEY(lizard_undo_retention_mutex, 0, 0, PSI_DOCUMENT_ME)};
 #endif /* UNIV_PFS_MUTEX */
 
 #ifdef UNIV_PFS_RWLOCK
@@ -23376,7 +23377,7 @@ static MYSQL_SYSVAR_ULONG(undo_retention,
                           lizard::Undo_retention::retention_time,
                           PLUGIN_VAR_OPCMDARG,
                           "Retention time of undo data in seconds", NULL,
-                          lizard::Undo_retention::on_update,
+                          lizard::Undo_retention::on_update_and_start,
                           0, 0, UINT_MAX32, 0);
 
 static MYSQL_SYSVAR_ULONG(undo_space_supremum_size,