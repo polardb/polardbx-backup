polardb/polardbx-engine

[Feature] SEQUENCE ENGINE
Description:
------------
SEQUENCE engine as an embedded logical engine, it's mainly used to
generate unique number, it is the middle layer engine that use InnoDB
or other storage engine as the based table engine, all query on sequence
table will be changed into the operation on based table, the cache or
other sequence value management is decided by SEQUENCE engine handler.

According to the setting which is defined by 'CREATE SEQUENCE ... ' or
'CREATE TABLE...Engine=Sequence + INSERT VALUES', user can query the
nextval or currval from sequence. In order to distinguish the normal
SELECT statement, we provide two functions

1. 'SELECT NEXTVAL FROM SEQUENCE' will return the base table record
2. 'SELECT NEXTVAL(), CURRVAL()' will return the iterated record

Besides the DIGITAL SEQUENCE (default), SEQUENCE ENGINE also has type
TIMESTAMP SEQUENCE, the format of generated TIMESTAMP value is

    [timstamp 42bits + sequence 16bits + unused 6bits]

Syntax:
-------
CREATE SEQUENCE SYNTAX:

    CREATE SEQUENCE [IF NOT EXISTS] schema.sequence_name
         [START WITH <constant>]
         [MINVALUE <constant>]
         [MAXVALUE <constant>]
         [INCREMENT BY <constant>]
         [CACHE <constant> | NOCACHE]
         [CYCLE | NOCYCLE]
         [TIMESTAMP] ;

    OR

    CREATE TABLE schema.sequence_name (
        `currval` bigint(21) NOT NULL COMMENT 'current value',
        `nextval` bigint(21) NOT NULL COMMENT 'next value',
        `minvalue` bigint(21) NOT NULL COMMENT 'min value',
        `maxvalue` bigint(21) NOT NULL COMMENT 'max value',
        `start` bigint(21) NOT NULL COMMENT 'start value',
        `increment` bigint(21) NOT NULL COMMENT 'increment value',
        `cache` bigint(21) NOT NULL COMMENT 'cache size',
        `cycle` bigint(21) NOT NULL COMMENT 'cycle state',
        `round` bigint(21) NOT NULL COMMENT 'already how many round'
    ) ENGINE=Sequence;

    INSERT INTO schema.sequence_name
         VALUES(0,0,1,9223372036854775

sql/sql_parse.cc
@@ -4198,6 +4198,8 @@ int mysql_execute_command(THD *thd, bool first_level) {
           DBUG_ASSERT(thd->slave_thread == 1);
           thd->security_context()->restore_security_context(thd, backup);
         }
+        if (lex->sphead->m_type == enum_sp_type::FUNCTION)
+          on_user_sp_status_changed(lex->sphead->m_db.str, name, true);
         my_ok(thd);
       }
       break; /* break super switch */
@@ -5830,6 +5832,7 @@ bool PT_common_table_expr::match_table_ref(TABLE_LIST *tl, bool in_self,
   @param partition_names
   @param option
   @param pc             Current parsing context, if available.
+  @param seq_scan_mode  Mode used to scan sequence table
 
   @return Pointer to TABLE_LIST element added to the total table list
   @retval
@@ -5840,11 +5843,13 @@ TABLE_LIST *SELECT_LEX::add_table_to_list(
     THD *thd, Table_ident *table_name, const char *alias, ulong table_options,
     thr_lock_type lock_type, enum_mdl_type mdl_type,
     List<Index_hint> *index_hints_arg, List<String> *partition_names,
-    LEX_STRING *option, Parse_context *pc) {
+    LEX_STRING *option, Parse_context *pc, Sequence_scan_mode seq_scan_mode) {
   TABLE_LIST *previous_table_ref =
       NULL; /* The table preceding the current one. */
   LEX *lex = thd->lex;
   DBUG_TRACE;
+
+  bool sequence_query;
 
   DBUG_ASSERT(table_name != nullptr);
   // A derived table has no table name, only an alias.
@@ -5904,6 +5909,8 @@ TABLE_LIST *SELECT_LEX::add_table_to_list(
     if (find_common_table_expr(thd, table_name, ptr, pc, &found_cte)) return 0;
     if (!found_cte && lex->copy_db_to(&ptr->db, &ptr->db_length)) return 0;
   }
+
+  sequence_query = (table_options & TL_OPTION_SEQUENCE);
 
   ptr->set_tableno(0);
   ptr->set_lock({lock_type, THR_DEFAULT});
@@ -5992,7 +5999,7 @@ TABLE_LIST *SELECT_LEX::add_table_to_list(
     }
   }
   /* Store the table reference preceding the current one. */
-  if (table_list.elements > 0) {
+  if (table_list.elements > 0 && !sequence_query) {
     /*
       table_list.next points to the last inserted TABLE_LIST->next_local'
       element
@@ -6017,7 +6024,8 @@ TABLE_LIST *SELECT_LEX::add_table_to_list(
     previous table reference to 'ptr'. Here we also add one element to the
     list 'table_list'.
   */
-  table_list.link_in_list(ptr, &ptr->next_local);
+  if (!sequence_query) table_list.link_in_list(ptr, &ptr->next_local);
+
   ptr->next_name_resolution_table = NULL;
   ptr->partition_names = partition_names;
   /* Link table in global list (all used tables) */
@@ -6064,6 +6072,8 @@ TABLE_LIST *SELECT_LEX::add_table_to_list(
       if (thd->is_error()) return nullptr;
     }
   }
+
+  ptr->sequence_scan.set(seq_scan_mode);
 
   return ptr;
 }

polardb/polardbx-backup

sql/sql_parse.cc
@@ -4428,6 +4428,8 @@ int mysql_execute_command(THD *thd, bool first_level) {
             thd->security_context()->restore_security_context(thd, backup);
           }
         }
+        if (lex->sphead->m_type == enum_sp_type::FUNCTION)
+          on_user_sp_status_changed(lex->sphead->m_db.str, name, true);
         my_ok(thd);
       }
       break; /* break super switch */
@@ -5887,6 +5889,7 @@ bool PT_common_table_expr::match_table_ref(Table_ref *tl, bool in_self,
   clause in statement
   @param option         Used by cache index
   @param pc             Current parsing context, if available.
+  @param seq_scan_mode  Mode used to scan sequence table
 
   @return Pointer to Table_ref element added to the total table list
   @retval
@@ -5897,11 +5900,13 @@ Table_ref *Query_block::add_table_to_list(
     THD *thd, Table_ident *table_name, const char *alias, ulong table_options,
     thr_lock_type lock_type, enum_mdl_type mdl_type,
     List<Index_hint> *index_hints_arg, List<String> *partition_names,
-    LEX_STRING *option, Parse_context *pc) {
+    LEX_STRING *option, Parse_context *pc, Sequence_scan_mode seq_scan_mode) {
   Table_ref *previous_table_ref =
       nullptr; /* The table preceding the current one. */
   LEX *lex = thd->lex;
   DBUG_TRACE;
+
+  bool sequence_query;
 
   assert(table_name != nullptr);
   // A derived table has no table name, only an alias.
@@ -5969,6 +5974,8 @@ Table_ref *Query_block::add_table_to_list(
     if (!found_cte && lex->copy_db_to(&ptr->db, &ptr->db_length))
       return nullptr;
   }
+
+  sequence_query = (table_options & TL_OPTION_SEQUENCE);
 
   ptr->set_tableno(0);
   ptr->set_lock({lock_type, THR_DEFAULT});
@@ -6062,7 +6069,7 @@ Table_ref *Query_block::add_table_to_list(
     }
   }
   /* Store the table reference preceding the current one. */
-  if (m_table_list.elements > 0) {
+  if (m_table_list.elements > 0 && !sequence_query) {
     /*
       table_list.next points to the last inserted Table_ref->next_local'
       element
@@ -6087,7 +6094,9 @@ Table_ref *Query_block::add_table_to_list(
     previous table reference to 'ptr'. Here we also add one element to the
     list 'table_list'.
   */
-  m_table_list.link_in_list(ptr, &ptr->next_local);
+  if (!sequence_query) m_table_list.link_in_list(ptr, &ptr->next_local);
+
+
   ptr->next_name_resolution_table = nullptr;
   ptr->partition_names = partition_names;
   /* Link table in global list (all used tables) */
@@ -6133,6 +6142,8 @@ Table_ref *Query_block::add_table_to_list(
       if (thd->is_error()) return nullptr;
     }
   }
+
+  ptr->sequence_scan.set(seq_scan_mode);
 
   return ptr;
 }