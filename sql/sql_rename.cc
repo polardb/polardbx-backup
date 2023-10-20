polardb/polardbx-engine

[fix] recycle_bin connsistency between master and slave
Summary
========
Forbid restore table to keep the recycle_bin independence between master and slave.
Since the table name generated in __recycle_bin__ can be different, which may cause
replication error when restore the table from __recycle_bin__.

In emergency, user with SUPER_ACL can still restore the table, but it will not
record binlog, so user should restore the table on master and slave separately.

sql/sql_rename.cc
@@ -148,12 +148,13 @@ static void find_and_set_explicit_duration_for_schema_mdl(
   @param thd          Thread context.
   @param table_list   Every two entries in the table_list form
                       a pair of original name and the new name.
+  @param is_restore   Is restore table, default false.
 
   @return True - on failure, false - on success.
 */
 
-bool mysql_rename_tables(THD *thd, TABLE_LIST *table_list) {
-  TABLE_LIST *ren_table = 0;
+bool mysql_rename_tables(THD *thd, TABLE_LIST *table_list, bool is_restore) {
+  TABLE_LIST *ren_table = nullptr;
   DBUG_TRACE;
 
   mysql_ha_rm_tables(thd, table_list);
@@ -499,7 +500,7 @@ bool mysql_rename_tables(THD *thd, TABLE_LIST *table_list) {
     }
   }
 
-  if (!error) my_ok(thd);
+  if (!error && !is_restore) my_ok(thd);
 
   return error;
 }

sql/sql_rename.h
@@ -26,6 +26,7 @@
 class THD;
 struct TABLE_LIST;
 
-bool mysql_rename_tables(THD *thd, TABLE_LIST *table_list);
+bool mysql_rename_tables(THD *thd, TABLE_LIST *table_list,
+                         bool is_restore = false);
 
 #endif /* SQL_RENAME_INCLUDED */

polardb/polardbx-backup

sql/sql_rename.cc
@@ -148,11 +148,12 @@ static void find_and_set_explicit_duration_for_schema_mdl(
   @param thd          Thread context.
   @param table_list   Every two entries in the table_list form
                       a pair of original name and the new name.
+  @param is_restore   Is restore table, default false.
 
   @return True - on failure, false - on success.
 */
 
-bool mysql_rename_tables(THD *thd, Table_ref *table_list) {
+bool mysql_rename_tables(THD *thd, Table_ref *table_list, bool is_restore) {
   Table_ref *ren_table = nullptr;
   DBUG_TRACE;
 
@@ -498,7 +499,7 @@ bool mysql_rename_tables(THD *thd, Table_ref *table_list) {
     }
   }
 
-  if (!error) my_ok(thd);
+  if (!error && !is_restore) my_ok(thd);
 
   return error;
 }

sql/sql_rename.h
@@ -26,6 +26,7 @@
 class THD;
 class Table_ref;
 
-bool mysql_rename_tables(THD *thd, Table_ref *table_list);
+bool mysql_rename_tables(THD *thd, Table_ref *table_list,
+                         bool is_restore = false);
 
 #endif /* SQL_RENAME_INCLUDED */